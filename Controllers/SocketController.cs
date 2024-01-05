25c79d0052f1db8421c9df7ab03b080789dca9a9
andreaquilau <andre.aquilau@outlook.com>
2024-01-04 23:34:28 -0400

    inital commit

---
+++ Controllers/SocketController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace WebSocketServer.Controllers;
+
+[ApiController]
+public class SocketController : ControllerBase
+{
+    [Route("/ws")]
+    public async Task Get()
+    {
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+
+            await Echo(webSocket);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+
+    private async Task Echo(WebSocket webSocket)
+    {
+        byte[] buffer = new byte[1024 * 24];
+
+        Console.WriteLine("Connection...");
+
+        var received = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        while (!received.CloseStatus.HasValue)
+        {
+            var str = Encoding.UTF8.GetString(buffer);
+            Console.WriteLine(str);
+
+            await webSocket.SendAsync(
+                new ArraySegment<byte>(buffer, 0, received.Count),
+                received.MessageType,
+                received.EndOfMessage,
+                CancellationToken.None
+            );
+
+            received = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        }
+
+        Console.WriteLine("Desconnect...");
+
+        await webSocket.CloseAsync(
+            received.CloseStatus.Value,
+            received.CloseStatusDescription,
+            CancellationToken.None
+        );
+
+    }
+}
